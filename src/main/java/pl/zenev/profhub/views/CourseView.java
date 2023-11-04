3593da011b79706c08635543e5e2f6f37fdd4cb7
Nick <zenevichnikita2001@gmail.com>
2023-11-04 18:37:32 +0100

    basic html page

---
+++ src/main/java/pl/zenev/profhub/views/CourseView.java
@@ -0,0 +1,24 @@
+package pl.zenev.profhub.views;
+
+
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import pl.zenev.profhub.services.CourseService;
+
+import java.io.IOException;
+import java.util.UUID;
+
+//@ViewScoped
+//@Named
+//public class CourseView {
+//    CourseService courseService;
+//    @Inject
+//    public CourseView(CourseService courseService) {
+//        this.courseService = courseService;
+//    }
+//    public void init() throws IOException {
+//
+//    }
+//}
