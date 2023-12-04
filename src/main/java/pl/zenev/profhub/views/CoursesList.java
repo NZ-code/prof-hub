f76b283babb481b74d120458636f133b0ae361b5
Nick <zenevichnikita2001@gmail.com>
2023-12-04 21:04:29 +0100

    logout working

--- src/main/java/pl/zenev/profhub/views/CoursesList.java
+++ src/main/java/pl/zenev/profhub/views/CoursesList.java
@@ -4,11 +4,14 @@ import jakarta.ejb.EJB;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
+import jakarta.security.enterprise.SecurityContext;
 import lombok.Getter;
 import pl.zenev.profhub.entities.Course;
 import pl.zenev.profhub.models.CoursesModel;
 import pl.zenev.profhub.helpers.CoursesToModel;
 import pl.zenev.profhub.services.CourseService;
+
+import static pl.zenev.profhub.security.UserRoles.ADMIN;
 
 @RequestScoped
 @Named
@@ -16,21 +19,27 @@ public class CoursesList {
     private  CourseService courseService;
     private CoursesModel coursesList;
     private CoursesToModel coursesToModel;
+    private final SecurityContext securityContext;
 
     @Getter
-    private boolean canDelete = false;
+    private boolean adminit = false;
 
     @EJB
-    public void setCourseService(CourseService courseService){
+    public void setCourseService(CourseService courseService ){
         this.courseService = courseService;
     }
     @Inject
-    public CoursesList( CoursesToModel coursesToModel){
+    public CoursesList( CoursesToModel coursesToModel, SecurityContext securityContext){
+        this.securityContext = securityContext;
         this.coursesToModel = coursesToModel;
     }
     public CoursesModel getCourses(){
         if(coursesList==null){
+
             coursesList = coursesToModel.apply(courseService.getAll());
+        }
+        if (securityContext.isCallerInRole(ADMIN)){
+            this.adminit = true;
         }
         return coursesList;
     }
